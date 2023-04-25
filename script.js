1ce6bb21f72f1e7caa465c8b00c2cfc0fb2528fc
yancis22 <131784864+yancis22@users.noreply.github.com>
2023-04-25 14:16:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let optionRight = document.querySelector (".option-one")
+let optionLeft = document.querySelector (".option-two")
+let optionElephant = document.querySelector (".option-one-screen")
+let optionTiger = document.querySelector (".option-two-screen")
+
+
+
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!*/
+optionRight.onclick=function(){
+optionElephant.style.display = "block"
+};
+
+optionLeft.onclick=function(){
+optionTiger.style.display = "block"
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
